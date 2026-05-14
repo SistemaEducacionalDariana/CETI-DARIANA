fdfabcead64e960d008c5b931de9fade996bfcd0
SistemaEducacionalDariana <professor.ermesonsouza@gmail.com>
2026-05-13 20:49:03 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,34 @@
-const CACHE = 'ceti-v1';
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/'])));
+const CACHE_NAME = 'ceti-dariana-v1';
+const ASSETS = [
+  '/CETI-DARIANA/',
+  '/CETI-DARIANA/index.html',
+  '/CETI-DARIANA/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
